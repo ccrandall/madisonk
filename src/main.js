4f49e0a03d2071cacc9e7b8a40839f51610459e4
Chad Crandall <ccrandall@users.noreply.github.com>
2020-06-09 08:59:00 -0700

    Vue fontawesome icon import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTwitter, faInstagram, faFacebook, faYoutube, faChevronUp, faChevronDown);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   render: h => h(App),
