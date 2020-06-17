498279c66dc8ef6c70d0b40255003b88ebfb7685
Chad Crandall <ccrandall@users.noreply.github.com>
2020-06-17 13:57:42 -0700

    header component updates, logo img

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,28 +1,24 @@
 
 <template>
     <header>
-        <div id="masthead" class="navbar menu-primary menu-light submenu-dark menu-transparent menu-add-padding style-light-original menu-absolute menu-with-logo" style="opacity: 1;">
+        <div id="masthead" class="navbar menu-primary">
             <div class="menu-container">
-                <div class="row-menu row-offcanvas">
-                    <div class="row-menu-inner row-brand menu-horizontal-inner">
-                        <div class="nav navbar-nav navbar-nav-first">
-                            <ul class="menu-smart sm menu-icons menu-smart-social">
-                                <li v-bind:key="link.id" v-for="link in socialLinks" class="menu-item-link social-icon tablet-hidden mobile-hidden"><a :href="link.href" class="social-menu-link" target="_blank"><font-awesome-icon :icon="[link.fontAwesomeClass, link.name]"></font-awesome-icon></a></li>
-                            </ul>
-                        </div>
-                        <div id="logo-container-mobile" class="col-lg-0 logo-container middle">
-                            <div id="main-logo" class="navbar-header style-light">
-                                <a href="#" class="navbar-brand" data-minheight="14">
-                                    <h2 class="text-logo h3 logo-skinnable main-logo" data-maxheight="14" style="font-size:14px;"></h2>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="mmb-container">
-                            <div class="mobile-menu-button 5 menu-button-overlay no-toggle mobile-menu-button-light lines-button x2 trigger-overlay" data-area="menu" data-container="main-container">
-                                <span class="lines"></span>
-                            </div>
-                        </div>
+                <div class="mmb-container">
+                    <div class="mobile-menu-button menu-button-overlay">
+                        <span class="lines"></span>
                     </div>
+                </div>
+                <div id="logo-container-mobile" class="logo-container">
+                    <div id="main-logo" class="navbar-header style-light">
+                        <a href="#" class="navbar-brand" data-minheight="14">
+                            <img :src="'./images/MadSoundzMusic_LOGO_BLACK.png'" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="nav navbar-nav social-icons-container">
+                    <ul class="menu-smart sm menu-icons menu-smart-social">
+                        <li v-bind:key="link.id" v-for="link in socialLinks" class="menu-item-link social-icon tablet-hidden mobile-hidden"><a :href="link.href" class="social-menu-link" target="_blank"><font-awesome-icon :icon="[link.fontAwesomeClass, link.name]"></font-awesome-icon></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -37,5 +33,103 @@ export default {
 </script>
 
 <style scoped>
+div#masthead {
+    position: relative;
+}
+.menu-container {
+    display: -ms-flexbox;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.menu-container > div {
+    width: 33%;
+    display: flex;
+}
+
+div#logo-container-mobile {
+    justify-content: center;
+}
+
+.row-menu {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.navbar-brand img {
+    width: 120px;
+    height: auto;
+}
+
+.social-icons-container {
+    justify-content: flex-end;
+}
+
+.menu-icons {
+    padding: 0;
+    display: inline;
+}
+
+ .menu-primary .menu-smart .social-icon {
+    display: inline-block;
+    padding: 10px;
+ }
+.menu-primary .menu-smart .social-icon a {
+    color: #000;
+}
+
+.mobile-menu-button {
+    display: block;
+}
+.lines-button {
+    transition: .3s;
+}
+.mobile-menu-button {
+    line-height: 0px;
+    /* transform: translateY(-50%); */
+    position: relative;
+    padding: 20px 36px 27px 36px;
+    cursor: pointer;
+}
+
+.lines {
+    position: relative;
+}
+
+.lines, .lines:before, .lines:after {
+    display: inline-block;
+    width: 20px;
+    height: 2px;
+    border-radius: 5px;
+    background: #000;
+}
+
+.lines:before, .lines:after {
+    position: absolute;
+    left: 0;
+    content: '';
+    -webkit-transform-origin: 10px center;
+    transform-origin: 10px center;
+}
+
+.lines:before {
+    top: 6px;
+}
+
+.lines:after {
+    top: -6px;
+}
+.lines:before, .lines:after {
+    transform-origin: 50% 50%;
+    transition: top 0.3s 0.6s ease-in-out,transform 0.3s ease-in-out;
+}
+
+@media (min-width: 960px) {
+    .menu-container {
+        padding: 0px 36px 0px 36px;
+    }
+}
 
 </style>
