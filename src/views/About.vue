98f209ef145b25f370b99ac90c93f109422c65d8
Chad Crandall <chad.crandall@ChadCrandalls-MacBook-Pro.local>
2020-06-22 15:25:55 -0700

    initial router config and view for routes

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1 class="text-center mt-4 mb-4">This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet iusto, earum quis suscipit molestiae quibusdam quae excepturi. Omnis repellat eligendi similique quae quibusdam autem debitis eveniet! Porro beatae nulla laudantium.</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  font-family: 'Playfair Display';
+}
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="contact">
+    <h1 class="text-center mt-4 mb-4">This is a contact page</h1>
+    <p>Contact information here</p>
+  </div>
+</template>
+
+<style scoped>
+.contact {
+  font-family: 'Playfair Display';
+}
+</style>
