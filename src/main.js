0b2ca8d09f838866dad0ec9a20a58f109ac32b41
Chad Crandall <chad.crandall@ChadCrandalls-MacBook-Pro.local>
2020-06-22 14:49:53 -0700

    add bootstrap vue to project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false;
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faTwitter, faInstagram, faFacebook, faYoutube, faChevronUp, faChevronDown);
+library.add(faTwitter, faInstagram, faFacebook, faYoutube, faChevronUp, faChevronDown, faTimes);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
