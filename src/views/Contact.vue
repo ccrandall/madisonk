335ff6d7b882874387a56827fba5b715dedca65d
Chad Crandall <ccrandall@users.noreply.github.com>
2020-08-08 13:49:10 -0700

    contact form submission

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,7 +5,7 @@
         <b-row class="mb-5">
           <b-col>
             <h1 class="text-center mt-4 mb-4">Contact</h1>
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form @submit="submitToAPI" @reset="onReset" v-if="show">
               <b-form-group
                 id="input-group-1"
                 label="Email:"
@@ -13,16 +13,17 @@
                 description=""
               >
                 <b-form-input
-                  id="input-1"
+                  id="email"
                   v-model="form.email"
                   type="email"
                   required
                   placeholder="Enter email"
+                  @blur="validateEmail"
                 ></b-form-input>
               </b-form-group>
               <b-form-group id="input-group-2" label="Name:" label-for="input-2">
                 <b-form-input
-                  id="input-2"
+                  id="name"
                   v-model="form.name"
                   required
                   placeholder="Enter name"
@@ -30,17 +31,20 @@
               </b-form-group>
               <b-form-group id="input-group-3" label="Message:" label-for="input-3">
                 <b-form-textarea
-                  id="textarea"
-                  v-model="text"
+                  id="message"
+                  v-model="form.message"
                   placeholder="Enter message..."
                   rows="3"
                   max-rows="6"
                 ></b-form-textarea>
               </b-form-group>
 
-              <b-button type="submit" variant="dark">Submit</b-button>
+              <b-button type="submit" variant="dark">Send</b-button>
             </b-form>
           </b-col>
+        </b-row>
+        <b-row class="mb-5" v-if="!show">
+          <b-col><h2 class="text-center">Thank you for contacting me! I'll be in touch soon.</h2></b-col>
         </b-row>
     </b-container>
   </div>
@@ -61,13 +65,84 @@ export default {
           name: '',
           message: ''
       },
-      show: true
+      show: true,
+      errors: {}
     }
   },
   methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
+      isValidEmail(email) {
+        var re = /^(([^<>()[]\\.,;:\s@"]+(\.[^<>()[]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      },
+      validateEmail: function() {
+        const isValid = this.isValidEmail(this.email);
+        this.errors.email = !isValid;
+      },
+      onReset(evt) {
+        console.log('onReset called');
+        evt.preventDefault();
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.message = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      },
+      submitToAPI(e) {
+        var contactForm = this;
+        console.log(contactForm);
+        e.preventDefault();
+        var URL = "https://ytbmeik2q5.execute-api.us-east-1.amazonaws.com/deploy1/contact";
+
+        var Namere = /[A-Za-z]{1}[A-Za-z]/;
+        if (!Namere.test(document.getElementById('name').value)) {
+            alert ("Name can not less than 2 char");
+            return;
+        }
+        
+        if (document.getElementById('email')=="") {
+            alert ("Please enter your email address");
+            return;
+        }
+
+        var reeamil = /^([\w-.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+        if (!reeamil.test(document.getElementById('email').value)) {
+            alert ("Please enter valid email address");
+            return;
+        }
+
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('message').value;
+        var data = {
+          name : name,
+          email : email,
+          message : message
+        };
+
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', URL);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var responseText = JSON.parse(xhr.responseText);
+                console.log('status 200: ', xhr);
+                console.log(responseText);
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('message').value = '';
+                contactForm.show = false
+                // contactForm.$nextTick(() => {
+                //   contactForm.show = true
+                // })
+            }
+        };
+        console.log(xhr);
+        xhr.send(JSON.stringify(data));
       }
     }
 }
