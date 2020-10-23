d5269230566b03f7267a7d68cce21c969cf18181
Chad Crandall <ccrandall@users.noreply.github.com>
2020-10-23 15:19:31 -0700

    link to youtube and linkedin, import those icons

--- src/main.js
+++ src/main.js
@@ -12,11 +12,13 @@ Vue.use(BootstrapVueIcons)
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimes, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faInstagram, faFacebook, faYoutube, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import router from './router'
 
-library.add(faTwitter, faInstagram, faFacebook, faYoutube, faChevronUp, faChevronDown, faTimes);
+// lookup icon imports here
+// https://github.com/FortAwesome/Font-Awesome/tree/master/js-packages/%40fortawesome/free-brands-svg-icons
+library.add(faTwitter, faInstagram, faFacebook, faYoutube, faChevronUp, faChevronDown, faTimes, faLinkedinIn);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
